484cb8e2ce5f8676b59ddd879646444ed51c37de
Ramez <ramez.festek@gmail.com>
2025-08-07 14:27:04 -0400

    added important Swiper css librariesto head of 'home' page. Also corrected swiper class <div> tag format.

--- javascript/swiper.js
+++ javascript/swiper.js
@@ -1,14 +1,35 @@
-// carousel of images for the home page
+// carousel component of images for the home page
 
-import Swiper from "swiper";
-import {Navigation, Pagination, Autoplay} from "swiper/modules";
+// Wait for DOM to be loaded
+document.addEventListener('DOMContentLoaded', function() {
+    // Initialize Swiper
+    const swiper = new Swiper('.swiper', {
+        modules: [Swiper.Navigation, Swiper.Pagination, Swiper.Autoplay],
+        loop: true,
+        autoplay: {
+            delay: 2500,
+            disableOnInteraction: false,
+        },
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        navigation: {
+            nextEl: '.swiper-button-next',
+            prevEl: '.swiper-button-prev',
+        },
+        // Responsive breakpoints
+        breakpoints: {
+            640: {
+                slidesPerView: 1,
+            },
+            768: {
+                slidesPerView: 1,
+            },
+            1024: {
+                slidesPerView: 1,
+            },
+        }
+    });
+});
 
-const swiper = new Swiper('.swiper', {
-    modules: [Navigation, Pagination, Autoplay],
-    loop: true,
-    autoplay: {
-        delay: 2500,
-        disableOnInteraction: false,
-    },
-    pagination: {}
-})
