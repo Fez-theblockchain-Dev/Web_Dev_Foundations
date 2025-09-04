51ad4ed2bd6ffb784de07303576d8f47fe39d5f7
Ramez <ramez.festek@gmail.com>
2025-09-04 15:50:35 -0400

    added new js file which will house the algorithm that randomly selects an index in the array displaying the workout assigned to that selected index. Also linked 'random.js at bottom of home.html page and created new <p> msg above where the 'workoutwheel will be placed. Currently has 2 ?? as placeholder

---
+++ javascript/random.js
@@ -0,0 +1,23 @@
+/* this file will be dedicated to building a spinning 'roulette wheel' 
+that will randomly select a workout for the user to do when they feel lost in the gym */
+
+
+// created array that will store the string values containing dif workouts a person can do
+const wheel = [
+    0 = "Barbell Squats 3 x 8 reps, 60% max weight", 
+    1 = "Incline dumbell bench press, 3 x 8 reps, 60% max weight",
+    2 = "Chest supported lat row (elbows 45 degrees from body), 3 x 8 reps, 60% max weight",
+    3 = "Ab circuit (crunches, scissor kicks, and windshield wipers) 3 x 20 seconds each excercise",
+    4 = "Barbell Front Squats 3 x 10 reps, 50% max weight",
+    5 = "Deadlifts 4 x 6-8 reps 70% max weight",
+    6 = "cable flys 3 x 8 reps, 75% max weight",
+    7 = "hamstring curl machine 3 x 10 reps, 50% max weight",
+    8 = "T-bar rows, 4 x 8 reps, 75% max weight",
+    9 = "Pull-ups 3 x AMRAP ('as many as possible')",
+    10 = "Walking lunges 3 x 20 reps, 20lb kettle bells each hand",
+    11 = "hip thrust machine 3 x 12 reps (Contract each rep at top)",
+    
+]
+
+// math.rand operator that I will be implementing for random index selection in the array
+
