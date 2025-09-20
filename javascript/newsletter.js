ba98d6282f74f1134eeb0345a4ae226f3337b0a8
Ramez <ramez.festek@gmail.com>
2025-09-20 14:37:09 -0400

    removed the Bootstrap CDN imports from my newsletter.js file. Also icluded important  declaration so that root styles alway take priority over css components.

--- javascript/newsletter.js
+++ javascript/newsletter.js
@@ -4,15 +4,11 @@
  * and adding them to a healthy lifestyle gym newsletter
  */
 
-// Bootstrap css library CDN
-<>/**
+/**
  * Newsletter Subscription Component
  * Reusable JavaScript component for collecting member/prospective member information
  * and adding them to a healthy lifestyle gym newsletter
  */
-    // Bootstrap css library CDN
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous"></link>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.bundle.min.js" integrity="sha384-FKyoEForCGlyvwx9Hj09JcYn3nv7wiPVlz7YYwJrWVcXK/BmnVDxM+D2scQbITxI" crossorigin="anonymous" defer></script></>
 
 
 class NewsletterComponent {
